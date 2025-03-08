ae5975f95e9fba5d75c80f19f94aad84d9ef6b3c
Noémie Gireaud <=>
2025-03-08 09:28:14 +0100

    [3] : local messaging finished

--- reseau-adhoc/TCP-execute/client.py
+++ reseau-adhoc/TCP-execute/client.py
@@ -1,40 +1,24 @@
-import sys
 import socket
 import json
 import os
-import time
+import argparse
 
-if len(sys.argv) != 6:
-    print("Usage : client.py local_ip local_port distant_ip distant_port filename")
-    sys.exit(0)
-
-
-local_ip = sys.argv[1]
-local_port = int(sys.argv[2])
-distant_ip = sys.argv[3]
-distant_port = int(sys.argv[4])
-file_to_send = sys.argv[5]
-try : 
-    file_size = os.path.getsize(file_to_send)
-except :
-    print("ERROR : file " + file_to_send + "not found")
-    exit(1)
+local_ip = None
+local_port = None
+distant_ip = None
+distant_port = None
+file_to_send = None
 
 def send_file_name(s, file_size) :
     data = json.dumps([local_ip, local_port, file_to_send, file_size])
     s.send(data.encode())
     
-    print("Sending Connexion Informations...")
-
 def send_file(s, file_size) :
-    print("Sending the file ...")
     with open(file_to_send, "rb") as file :
         send_bytes = s.sendfile(file)
         if(send_bytes != file_size) :
             print("ERROR : filesize = ", file_size, " send_bytes = ", send_bytes)
             exit(1)
-        print("Transmission success ! (Yay)")
-
 
 def read_response(clientsocket):
 
@@ -44,30 +28,89 @@ def read_response(clientsocket):
 
     return bool(json.loads(data.decode()))
 
-
 def response_ok():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
         serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         serversocket.bind(('', local_port))
         serversocket.listen(10)
 
-        print("Waiting for server connection response...",  flush=True)
         (clientsocket, address) = serversocket.accept()
         return read_response(clientsocket)
 
-def send_executable():
+def send_executable(file_size):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print("Connection to Socket ...")
         s.connect((distant_ip, distant_port))
-        
+
+        print("--Sending Connexion Informations...")
         send_file_name(s, file_size)
 
+        print("--Waiting for server ack ...",  flush=True)
         if response_ok() :
+            print("--Sending the file ...")
             send_file(s, file_size)
         else : 
             print("ERROR : server could not read file")
 
-        print("Connection to Socket Closed")
-        
+    print("--Connection to Socket Closed")
 
-send_executable()
+def send_command_instruction(command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print("Connection to Socket ...")
+        s.connect((distant_ip, distant_port))
+
+        print("--Sending command instruction...")
+        data = json.dumps([command])
+        s.send(data.encode())
+
+    print("--Connection to Socket Closed")
+
+
+def parse_file(file):
+    global file_to_send
+    global local_ip
+    global local_port
+
+    file_to_send = file
+    local_ip = "localhost"
+    local_port = 9000 + os.getpid()%1000
+    print("local_port = ", local_port)
+
+    try : 
+        file_size = os.path.getsize(file_to_send)
+    except :
+        print("ERROR : file " + file_to_send + " not found")
+        exit(1)
+
+    send_executable(file_size)
+
+
+
+def parse_command(command):
+    command = (" ").join(command)
+    send_command_instruction(command)
+
+
+
+def parse_args():
+    global distant_ip
+    global distant_port
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('distant_ip', type=str)
+    parser.add_argument('distant_port', type=int)
+    parser.add_argument('-c', '--command', nargs='+', type=str, metavar=('command'))
+    parser.add_argument('-f', '--file', type=str, metavar=('file_path'))
+
+    args = parser.parse_args()
+    distant_ip = args.distant_ip
+    distant_port = args.distant_port
+
+    if(args.file) :
+        parse_file(args.file)
+
+    if(args.command) :
+        parse_command(args.command)
+
+
+parse_args()  
--- reseau-adhoc/TCP-execute/server.py
+++ reseau-adhoc/TCP-execute/server.py
@@ -1,10 +1,8 @@
 import sys
 import socket
 import json
-
-if len(sys.argv) != 3:
-    print("Usage : server.py local_ip local_port")
-    sys.exit(0)
+import argparse
+import subprocess
 
 local_ip = sys.argv[1]
 local_port = int(sys.argv[2])
@@ -37,21 +35,54 @@ def send_ack(distant_ip, distant_port, ack):
         data = json.dumps([ack])
         s.send(data.encode())
 
+def receive_file(clientsocket, distant_ip, distant_port, file_name, file_size):
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
-        serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        serversocket.bind(('', local_port))
-        serversocket.listen(10)
+    print("--Sending ack ...")
+    send_ack(distant_ip, distant_port, True)
 
-        while True :
-            print("Waiting for connection requests ...",  flush=True)
-            (clientsocket, address) = serversocket.accept()
+    print("--Receiving file content ... ")
+    write_file(clientsocket, file_name, file_size)
 
-            print("Connection request received, decoding of connexion data ...")
-            distant_ip, distant_port, file_name, file_size = get_file_infos(clientsocket)
+def execute_command(command):
+    print("-- Executing command : " + command)
+    subprocess.Popen(command, shell=True)
 
-            print("Sending ack ...")
-            send_ack(distant_ip, distant_port, True)
+def listening_loop():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+            serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            serversocket.bind(('', local_port))
+            serversocket.listen(10)
 
-            print("Receiving file content ... ")
-            write_file(clientsocket, file_name, file_size)
+            while True :
+                print("Waiting for connection requests ...",  flush=True)
+                (clientsocket, address) = serversocket.accept()
+
+                print("--Connection request received, decoding the connexion data ...")
+                infos = get_file_infos(clientsocket)
+                match len(infos):
+
+                    case 1 : 
+                        execute_command(infos[0])
+                    case 4 : 
+                        receive_file(clientsocket, infos[0], infos[1], infos[2], infos[3])
+                    case _ : 
+                        print("ERROR : websocket connexion data does not match usage ")
+                        print("usage1: command ")
+                        print("usage2: local_ip, local_port, file_to_send, file_size ")
+
+
+def parse_args():
+    global local_ip
+    global local_port
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('local_ip', type=str)
+    parser.add_argument('local_port', type=int)
+    args = parser.parse_args()
+    local_ip = args.local_ip
+    local_port = args.local_port
+
+    listening_loop()
+
+
+parse_args()  
