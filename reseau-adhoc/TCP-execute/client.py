131acf6e0e63b2778eec714f6ffb1237dc214ec0
Noémie Gireaud <=>
2025-03-14 11:47:40 +0100

    E[6] : DONE. ( to test with ad-hoc and distances )

--- reseau-adhoc/TCP-execute/client.py
+++ reseau-adhoc/TCP-execute/client.py
@@ -2,6 +2,7 @@ import socket
 import json
 import os
 import argparse
+import time
 
 local_ip = None
 local_port = None
@@ -9,62 +10,79 @@ distant_ip = None
 distant_port = None
 file_to_send = None
 
+MAX_LOOP = 100
+WAITING_TIME=1
+
 def send_file_name(s, file_size) :
     data = json.dumps([local_ip, local_port, file_to_send, file_size])
     s.send(data.encode())
     
-def send_file(s, file_size) :
+def send_file(s, file_size, received_bytes) :
     with open(file_to_send, "rb") as file :
-        send_bytes = s.sendfile(file)
-        if(send_bytes != file_size) :
+        send_bytes = s.sendfile(file, offset=received_bytes)
+        if(send_bytes != file_size-received_bytes) :
             print("ERROR : filesize = ", file_size, " send_bytes = ", send_bytes)
             exit(1)
+    
 
-def read_response(clientsocket):
-
-    data = clientsocket.recv(1024)
-    if data == b'':
-        print("ERROR : Empty response given ! ")
-
-    return bool(json.loads(data.decode()))
-
-def response_ok():
+def receive_server_response():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+        
         serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         serversocket.bind(('', local_port))
         serversocket.listen(10)
+        serversocket.settimeout(3)
 
-        (clientsocket, address) = serversocket.accept()
-        return read_response(clientsocket)
+        try: 
+            (clientsocket, address) = serversocket.accept()
+            data = clientsocket.recv(1024)
+            if data == b'':
+                print("ERROR : Empty response given ! ")
 
+            ack, received_bytes = json.loads(data.decode())
+            return bool(ack), received_bytes
+        except socket.timeout:
+            print("ACK TIMEOUT")
+            return False
+    
+
+# 
 def send_executable(file_size):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        print("Connection to Socket ...")
-        s.connect((distant_ip, distant_port))
+    compt = 0
+    received_bytes = 0
+    send_complete = False
 
-        print("--Sending Connexion Informations...")
-        send_file_name(s, file_size)
+    while (received_bytes < file_size and compt < MAX_LOOP) : 
+        try : 
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((distant_ip, distant_port))
 
-        print("--Waiting for server ack ...",  flush=True)
-        if response_ok() :
-            print("--Sending the file ...")
-            send_file(s, file_size)
-        else : 
-            print("ERROR : server could not read file")
+                print("Requesting ack ...")
+                send_file_name(s, file_size)
 
-    print("--Connection to Socket Closed")
+                ack, received_bytes = receive_server_response()
+                print("---> Server ack received, it is waiting for ", file_size-received_bytes, " bytes",  flush=True)
+                if ack and received_bytes != file_size :
+                    print("Sending file bytes ...")
+                    send_file(s, file_size, received_bytes)
+        except :  
+            print("---> Connexion Closed, trying to send again in ", WAITING_TIME, " seconds ...")
+            compt += 1
+            time.sleep(WAITING_TIME)
+
+    if received_bytes == file_size : 
+        print("---> Transmission Successful !")
+    else : 
+        print("---> Transmission Failure")
 
 def send_command_instruction(command):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        print("Connection to Socket ...")
         s.connect((distant_ip, distant_port))
 
         print("--Sending command instruction...")
         data = json.dumps([command])
         s.send(data.encode())
-
-    print("--Connection to Socket Closed")
-
+        print("---> command well send ! ")
 
 def parse_file(file):
     global file_to_send
