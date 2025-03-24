62c741e6d5704fcb07ceb5103b5fba11b3f5f262
Daoud LAMALMI <daoudlamalmi@proton.me>
2025-03-24 18:57:48 +0100

    [E5] : first test for bluetooth commands

---
+++ bluetooth-command/src/main.rs
@@ -0,0 +1,44 @@
+use std::error::Error;
+
+use bluest::Adapter;
+use futures_lite::StreamExt;
+use tracing::info;
+use tracing::metadata::LevelFilter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    use tracing_subscriber::prelude::*;
+    use tracing_subscriber::{fmt, EnvFilter};
+
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    let adapter = Adapter::default()
+        .await
+        .ok_or("Bluetooth adapter not found")?;
+    adapter.wait_available().await?;
+
+    info!("starting scan");
+    let mut scan = adapter.scan(&[]).await?;
+    info!("scan started");
+    while let Some(discovered_device) = scan.next().await {
+        info!(
+            "{}{}: {:?}",
+            discovered_device
+                .device
+                .name()
+                .as_deref()
+                .unwrap_or("(unknown)"),
+            discovered_device.device.id(),
+            discovered_device.adv_data,
+        );
+    }
+
+    Ok(())
+}
