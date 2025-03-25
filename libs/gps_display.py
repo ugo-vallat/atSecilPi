383a4b28b485a5c192b925441f197fc2bdfa3238
Noémie Gireaud <=>
2025-03-25 21:57:01 +0100

    [E7] : display finished !

--- libs/gps_display.py
+++ libs/gps_display.py
@@ -1,55 +1,114 @@
-import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 from gps_simulator import *
 from enum import Enum
 import matplotlib.cm as cm
+from colorist import ColorRGB
+
 
 from libgps import *
 from libnetwork import *
 
 
-rainbow_colors = cm.rainbow(np.linspace(0, 1, 100))
 
-
+#------------------ Network connexion ---------------------
 class Mode(Enum):
     PI=1
     LOCAL_HOST=2
 
 MODE = Mode.LOCAL_HOST
 ID = 1
-matplotlib.interactive(True)
-LIMIT = 3
+network = AdhocNetwork(id=ID, local_host=(MODE == Mode.LOCAL_HOST))
+
+#----------------------------------------------------------
+rainbow_colors = cm.rainbow(np.linspace(0, 1, 20))
+rainbow_colors = np.delete(rainbow_colors, 3, axis=1 )
+rainbow_colors = 255 * rainbow_colors
+
+LIMIT = 5
+
+TRUE_SIZE = 10
+SIZE = TRUE_SIZE *2
+
+points = []
+colors = []
+
+def rgb_color(color, text):
+    r, g, b = color.astype(int)
+    real_color = ColorRGB(r, g, b)
+
+    colored_text = f"{real_color}{text}{real_color.OFF}"
+    return colored_text
 
 
-fig = plt.figure()
-subplot = fig.add_subplot(1,1,1)
+def create_grid():
+    return [[" " for _ in range(SIZE)] for _ in range(SIZE)]
 
-scatter_plot = subplot.scatter([], [], edgecolor='black')
+def print_grid(grid):
+    print("       " + "   ".join( str(i+1) for i in range(TRUE_SIZE)))
+    print("    +" + "----" * TRUE_SIZE + "+")
 
-plt.show()
+    for i, row in enumerate(grid):
+        i = i+1
+        if i%2 != 0 : 
+            print("    |" + " ".join(row) + " |") 
+        else : 
+            print("{: >3d} |".format(int((i+1)/2)) + " ".join(row) + " |") 
+    print("    +" + "----" * TRUE_SIZE + "+")
+
+def clear_plot():
+    global grid
+    for _ in range(SIZE+4):
+        print("\033[F", end='')
+        print("\033[K", end='')
+
+    grid = create_grid()
+
+# Initialize the grid
+grid = create_grid()
+
+
+
+# Example: Place an 'O' at (4, 5)
+
+def place_marker(grid, row, col, color, marker="O",):
+    grid[row][col] = rgb_color(color, marker)
+
+# Print the grid
+print_grid(grid)
+
 
 def display(fix, lat, lon, altitude):
+    global points
+    global colors
+
     if(fix > 0):
+        
+        clear_plot()
 
-        offset =  scatter_plot.get_offsets().data
-        offset = np.append(offset, [(lat, lon)], axis=0)
-        if np.shape(offset)[0] > LIMIT : 
-            offset = np.delete(offset, 0, axis=0)
+        points.append((lat, lon))
+        colors.append(rainbow_colors[int(altitude*2)-1])
 
-        scatter_plot.set_offsets(offset)
+        if len(points) > LIMIT : 
+            points = list(np.delete(points, 0, axis=0))
+            colors = list(np.delete(colors, 0, axis=0))
+        
+        for i, point in enumerate(points) : 
+            lat =  int(point[0]*2)-1
+            lon = int(point[1]*2)-1
+            color = colors[i]
+            if (i == len(points) -1):
+                place_marker(grid,  lat, lon, color, "@")
+            elif i == len(points) -1 or  i == len(points) -2 :
+                place_marker(grid, lat, lon, color, "O")
+            elif (i == 0) :
+                place_marker(grid, lat, lon, color, ".")
+            else :
+                place_marker(grid, lat, lon, color, "o")
+           
 
-        colors = scatter_plot.get_facecolors()
-        colors = np.append(colors, [rainbow_colors[int(altitude*10)]], axis=0)
+        print_grid(grid)
 
-        while np.shape(colors)[0] > np.shape(offset)[0]:
-            colors = np.delete(colors, 0, axis=0)
 
-        scatter_plot.set_facecolor(colors)
-
-        scatter_plot.changed()
-
-network = AdhocNetwork(id=ID, local_host=(MODE == Mode.LOCAL_HOST))
 
 def adhoc_receiver(network:AdhocNetwork):
     printl("start...")
@@ -60,10 +119,5 @@ def adhoc_receiver(network:AdhocNetwork):
         print(f"[gps_app.adhoc_receiver] received pos \t : {pos}")
         display(fix, lat, lon, altitude)
 
-"""
-OK- fix : 0 = pas de données (si on veut simuler l'absence de données, 1 ou 2 = données gps
-lat : latitude (coordonnées cartésiennes)
-lon = longitude  (coordonnées cartésiennes)
-alt = altitude  (coordonnées cartésiennes)
-"""
+
 adhoc_receiver(network)
