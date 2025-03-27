2c85abf4c1d1329c2bb2fbb8e3326ab54b025cba
Ugo Vallat <ugo.vallat.pro@gmail.com>
2025-03-27 13:09:23 +0100

    [E1] : improv script param

--- reseau-adhoc/e1-param-reseau.sh
+++ reseau-adhoc/e1-param-reseau.sh
@@ -4,6 +4,7 @@ IFACE="wlan0"
 
 # Informations détaillées sur les interfaces réseau
 echo "Interfaces réseau actives :"
+echo "\033[38;5;45m $ ip -br address\033[0m"
 ip -br address
 echo -e "\n+--------------------------------------------------+\n"
 
@@ -18,26 +19,31 @@ echo -e "\n+--------------------------------------------------+\n"
 
 # Infos générales sur l'interface
 echo "État de l'interface $IFACE :"
+echo "\033[38;5;45m $ iwconfig $IFACE; iwlist $IFACE channel | grep Current Frequency \033[0m"
 iwconfig "$IFACE"
 iwlist "$IFACE" channel | grep "Current Frequency"
 echo -e "\n+--------------------------------------------------+\n"
 
 # Canaux WiFi utilisés
 echo "Liste des canaux disponnibles :"
+echo "\033[38;5;45m $ \033[0m"
 iwlist  channel
 echo -e "\n+--------------------------------------------------+\n"
 
 # Scan des réseaux pour voir les canaux et interférences
 echo "Scan des canaux wifi :"
+echo "\033[38;5;45m $ sudo iwlist $IFACE scan | grep -E Cell|Frequency|Quality|ESSID \033[0m"
 sudo iwlist "$IFACE" scan | grep -E "Cell|Frequency|Quality|ESSID" | sed 's/^[ \t]*//'
 echo -e "\n+--------------------------------------------------+\n"
 
 # Statistiques TX/RX
 echo "Statistiques de l'interface :"
+echo "\033[38;5;45m $ cat /proc/net/dev \033[0m"
 cat /proc/net/dev 
 echo -e "\n+--------------------------------------------------+\n"
 
 # Taux de perte de paquets
 echo "Taux de perte de paquets :"
+echo "\033[38;5;45m $ ping -c 10 google.com | grep -oP '\d+(?=% packet loss)' \033[0m"
 ping -c 10 google.com | grep -oP '\d+(?=% packet loss)'
 echo -e "\n+--------------------------------------------------+\n"
