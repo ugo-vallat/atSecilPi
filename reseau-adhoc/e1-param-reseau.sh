b02fd82b4ac9617f04bc10fad625e97807ad699e
Ugo Vallat <ugo.vallat.pro@gmail.com>
2025-03-27 09:45:42 +0100

    [E1] : improvement script param

--- reseau-adhoc/e1-param-reseau.sh
+++ reseau-adhoc/e1-param-reseau.sh
@@ -2,50 +2,42 @@
 
 IFACE="wlan0"
 
+# Informations détaillées sur les interfaces réseau
+echo "Interfaces réseau actives :"
+ip -br address
+echo -e "\n+--------------------------------------------------+\n"
+
 # Vérifie que l'interface existe
+echo -n "Recherche interface $IFACE... "
 if ! ip link show "$IFACE" &> /dev/null; then
-    echo "Interface $IFACE non trouvée"
+    echo -e "\033[38;5;196mKO\033[0m"
     exit 1
 fi
+echo -e "\033[38;5;46mOK\033[0m"
+echo -e "\n+--------------------------------------------------+\n"
 
 # Infos générales sur l'interface
 echo "État de l'interface $IFACE :"
 iwconfig "$IFACE"
-echo ""
+iwlist "$IFACE" channel | grep "Current Frequency"
+echo -e "\n+--------------------------------------------------+\n"
+
+# Canaux WiFi utilisés
+echo "Liste des canaux disponnibles :"
+iwlist  channel
+echo -e "\n+--------------------------------------------------+\n"
 
 # Scan des réseaux pour voir les canaux et interférences
-echo "Scan des réseaux WiFi (canaux et qualité) :"
+echo "Scan des canaux wifi :"
 sudo iwlist "$IFACE" scan | grep -E "Cell|Frequency|Quality|ESSID" | sed 's/^[ \t]*//'
-echo ""
-
-# Affichage du canal actuel
-echo "Canal utilisé par $IFACE :"
-iwlist "$IFACE" channel | grep "Current Frequency"
-echo ""
-
-# Informations détaillées sur les interfaces réseau
-echo "Interfaces réseau actives :"
-ip -br address
-echo ""
+echo -e "\n+--------------------------------------------------+\n"
 
 # Statistiques TX/RX
 echo "Statistiques de l'interface :"
 cat /proc/net/dev 
-echo ""
-
-#----------------------------------------------------------------------
-
-# Canaux WiFi utilisés
-echo "Canaux WiFi utilisés :"
-iwlist  channel
-echo ""
-
-# Interférences WiFi (Autres réseaux sur le même canal)
-echo "Interférences WiFi (Autres réseaux sur le même canal) :"
-iwlist wlan0 scan | grep -E "Channel|ESSID"
-echo ""
+echo -e "\n+--------------------------------------------------+\n"
 
 # Taux de perte de paquets
 echo "Taux de perte de paquets :"
 ping -c 10 google.com | grep -oP '\d+(?=% packet loss)'
-echo ""
+echo -e "\n+--------------------------------------------------+\n"
